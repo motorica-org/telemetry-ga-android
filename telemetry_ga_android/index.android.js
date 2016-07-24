dba02ee00ddb35256a960c5b5849046d28e7633d
Dmitriy Volkov <wldhx@wldhx.me>
2016-07-24 11:06:45 +0300

    Implement SwitchingComponent
    Switches between given components by timer.

--- telemetry_ga_android/index.android.js
+++ telemetry_ga_android/index.android.js
@@ -9,7 +9,8 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
 
 class telemetry_ga_android extends Component {
@@ -25,6 +26,16 @@ class telemetry_ga_android extends Component {
         <Text style={styles.instructions}>
           Shake or press menu button for dev menu
         </Text>
+	<SwitchingComponent
+          sources={[
+            <Image source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+	                    style={{width: 400, height: 400}} />,
+            <Image source={{uri: 'http://facebook.github.io/react/img/logo_small.png'}}
+			    style={{width: 400, height: 400}} />,
+            <Image source={{uri: 'http://facebook.github.io/react/img/logo_small_2x.png'}}
+			    style={{width: 400, height: 400}} />,
+          ]}
+        />
       </View>
     );
   }
@@ -48,5 +59,29 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+class SwitchingComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    const sources = this.props.sources; // FIXME: why do we need to copy here?
+    this.state = { current_component: sources[0] };
+
+    function* getNewComponent() {
+      while (true) {
+        for (let i of sources) {
+          yield i;
+        }
+      }
+    }
+
+    let g = getNewComponent();
+    setInterval(() => { this.setState({ current_component: g.next().value }); }, 500);
+  }
+
+  render() {
+      return this.state.current_component
+  }
+}
 
 AppRegistry.registerComponent('telemetry_ga_android', () => telemetry_ga_android);
