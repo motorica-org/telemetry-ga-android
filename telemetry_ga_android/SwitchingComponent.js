e44b039ba0e1285effd889a558dcf5f41b868747
Dmitriy Volkov <wldhx@wldhx.me>
2016-08-23 13:24:01 +0300

    refactor(*): Move SwitchingComponent to separate file

---
+++ telemetry_ga_android/SwitchingComponent.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { NativeAppEventEmitter } from 'react-native';
+
+
+class SwitchingComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const sources = this.props.sources; // FIXME: why do we need to copy here?
+    this.state = {
+      current_component: sources[0],
+    };
+
+    function* getNewComponent() {
+      while (true) {
+        for (const i of sources) {
+          yield i;
+        }
+      }
+    }
+
+    const g = getNewComponent();
+    NativeAppEventEmitter.addListener('BleManagerDidUpdateValueForCharacteristic',
+      () => {
+        this.setState({
+          current_component: g.next().value,
+        });
+      }
+    );
+  }
+
+  render() {
+    return this.state.current_component;
+  }
+}
+
+module.exports = SwitchingComponent;
--- telemetry_ga_android/index.android.js
+++ telemetry_ga_android/index.android.js
@@ -8,6 +8,7 @@ import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View, NativeAppEventEmitter, Image } from 'react-native';
 import BleManager from 'react-native-ble-manager';
 
+import SwitchingComponent from './SwitchingComponent';
 import PulsingImage from './PulsingImage';
 
 
@@ -53,38 +54,6 @@ class telemetry_ga_android extends Component {
         />
       </View>
       );
-  }
-}
-
-class SwitchingComponent extends Component {
-  constructor(props) {
-    super(props);
-
-    const sources = this.props.sources; // FIXME: why do we need to copy here?
-    this.state = {
-      current_component: sources[0],
-    };
-
-    function* getNewComponent() {
-      while (true) {
-        for (const i of sources) {
-          yield i;
-        }
-      }
-    }
-
-    const g = getNewComponent();
-    NativeAppEventEmitter.addListener('BleManagerDidUpdateValueForCharacteristic',
-      () => {
-        this.setState({
-          current_component: g.next().value,
-        });
-      }
-    );
-  }
-
-  render() {
-    return this.state.current_component;
   }
 }
 
