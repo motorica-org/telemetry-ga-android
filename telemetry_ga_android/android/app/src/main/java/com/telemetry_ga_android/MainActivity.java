c04ac8b54ab7779c398dd63df13b9f7122645acc
Dmitriy Volkov <wldhx@wldhx.me>
2016-09-18 16:53:34 +0300

    feat(view): Enter Immersive mode

--- telemetry_ga_android/android/app/src/main/java/com/telemetry_ga_android/MainActivity.java
+++ telemetry_ga_android/android/app/src/main/java/com/telemetry_ga_android/MainActivity.java
@@ -1,5 +1,7 @@
 package com.telemetry_ga_android;
 
+import android.view.View;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +13,17 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "telemetry_ga_android";
+    }
+
+    @Override
+    protected void onStart() {
+	    getWindow().getDecorView().setSystemUiVisibility(
+			      View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+			    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+			    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+			    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide navigation bar
+			    | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+			    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+	    super.onStart();
     }
 }
