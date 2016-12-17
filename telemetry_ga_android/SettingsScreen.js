cb8a735591dcea7d7cafc019c046d949d8a22a0e
Dmitriy Volkov <wldhx@wldhx.me>
2016-12-17 23:05:15 +0300

    feat(view): Add a navbar and buttons to Settings page

--- telemetry_ga_android/SettingsScreen.js
+++ telemetry_ga_android/SettingsScreen.js
@@ -1,6 +1,28 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Button } from 'react-native';
 
 
-export default () =>
-  <Text>I am Settings, and I set!</Text>;
+export default class extends React.Component {
+  static route = {
+    navigationBar: {
+      title: 'Settings',
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Button
+          onPress={() => this.props.navigator.push(this.props.navigator.router.getRoute('main'))}
+          title="Scan QR-encoded settings"
+          color="#303f9f"
+        />
+        <Button
+          onPress={() => this.props.navigator.push(this.props.navigator.router.getRoute('main'))}
+          title="Connect the prosthetic"
+          color="#7b1fa2"
+        />
+      </View>
+    );
+  }
+}
