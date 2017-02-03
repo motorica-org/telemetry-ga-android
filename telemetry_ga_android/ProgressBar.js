c713cf488b5181907d56419e5364cdb9bd506ce3
Dmitriy Volkov <wldhx@wldhx.me>
2017-02-03 19:16:47 +0300

    feat(flow): Annotate ProgressBar

--- telemetry_ga_android/ProgressBar.js
+++ telemetry_ga_android/ProgressBar.js
@@ -1,17 +1,21 @@
+// @flow
 import React from 'react';
 import { View } from 'react-native';
+import type { StyleSheet } from 'react-native';
 
 
-export default (props) => {
-  const size = (props.flex_count % 25) / 25;
+export default ({ flex_count, style }:
+                { flex_count: number,
+                  style: StyleSheet }) => {
+  const size = (flex_count % 25) / 25;
 
   return (
     <View style={{ flex: 1 }}>
-      <View style={{ flex: 1 - size, backgroundColor: props.style.backgroundColor, opacity: 0.2 }} />
+      <View style={{ flex: 1 - size, backgroundColor: style.backgroundColor, opacity: 0.2 }} />
       <View
         style={{
           flex: size,
-          ...props.style,
+          ...style,
         }}
       />
     </View>
